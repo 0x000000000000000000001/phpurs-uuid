a0a243ada71d5760e33e0caa7bad4794765b0be4
0x1 <kevin.francart3@gmail.com>
2026-07-11 13:36:07 +0200

    chore: align phpurs ecosystem to latest features and performance optimizations

--- src/Data/UUID.php
+++ src/Data/UUID.php
@@ -1,8 +1,28 @@
 <?php
 
-
-use Symfony\Component\Uid\Uuid;
-
-$Data_UUID_getUUIDImpl = function() {
-    return Uuid::v4()->toRfc4122();
+$getUUIDImpl = function() {
+    return \Symfony\Component\Uid\Uuid::v4()->toRfc4122();
 };
+
+$validateV4UUID = function($str) {
+    return \Symfony\Component\Uid\Uuid::isValid($str);
+};
+
+$getUUID3Impl = function($str) {
+    return function($namespace) use ($str) {
+        return \Symfony\Component\Uid\Uuid::v3(\Symfony\Component\Uid\Uuid::fromString($namespace), $str)->toRfc4122();
+    };
+};
+
+$getUUID5Impl = function($str) {
+    return function($namespace) use ($str) {
+        return \Symfony\Component\Uid\Uuid::v5(\Symfony\Component\Uid\Uuid::fromString($namespace), $str)->toRfc4122();
+    };
+};
+
+return [
+    'getUUIDImpl' => $getUUIDImpl,
+    'validateV4UUID' => $validateV4UUID,
+    'getUUID3Impl' => $getUUID3Impl,
+    'getUUID5Impl' => $getUUID5Impl
+];
